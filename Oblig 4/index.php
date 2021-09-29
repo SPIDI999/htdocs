b91ed3dbc95b2d85fb455b5fbbddb5ca40fef846
SPIDI999 <szymonrys@op.pl>
2021-09-29 12:36:52 +0200

    Inittial Commit

--- Oblig 4/index.php
+++ Oblig 4/index.php
@@ -11,7 +11,7 @@
 <article>
     <h1>Oppgave 1a) og b)</h1>
         <form action="index.php" method="post">
-            <p><input type="text" id="name" name="name" required> Ditt navn<br></p>
+            <p><input type="text" id="name" name="name" > Ditt navn<br></p>
             <a><input type="submit" value="submit"></a>
             
     <ul>
@@ -68,7 +68,48 @@ for($i=0; $i<13; $i++){
 
 
 <h1>Oppgave 6</h1>
+<form action="index.php" method="post">
+            <input type="text" id="name" name="name"> Navn<br>
+            <input type="text" id="ad" name="ad"> Adresse<br>
 
+            <a><input type="radio" name="car" value="henting"> Hente selv<br></a>
+            <a><input type="radio" name="car" value="levering"> Leveranse (+65kr)<br></a>
+
+            <ul>
+                <label for="pizza">Velg pizza-type</label>  
+                    <select id="pizza" name="pizza">
+                        <a><option value="Hawaisk Pizza"> Hawaisk Pizza</option></a>
+                        <a><option value="Miesny Jez"> Miesny Jez</option></a>
+                        <a><option value="Smol Terry"> Smol Terry</option></a>
+                    </select>
+            </ul>
+
+            <a><input type="radio" name="ost" value="Normal Ost"> Normal Ost<br></a>
+            <a><input type="radio" name="ost" value="Ekstra Ost"> Ekstra Ost<br></a>
+
+            <p><input type="submit" value="submit"></p>
+
+<?php 
+
+if (isset($_POST["name"]) && isset($_POST["car"]) && isset($_POST["pizza"]) && isset($_POST["ost"]) && isset($_POST["ad"])){
+
+    $name = $_POST["name"];
+    $car = $_POST["car"];
+    $pizza = $_POST["pizza"];
+    $ost = $_POST["ost"];
+    $ad = $_POST["ad"];
+
+    echo "Takk for din bestilling, $name <br>";
+    echo "Du har valgt $pizza med $ost <br>";
+    echo "Leveres til $ad <br>";
+
+}
+
+
+
+
+
+?>
 
 <h2><p><a href="../index.php">Tilbake til hovedsiden</a></p></h2>    
 </body>
