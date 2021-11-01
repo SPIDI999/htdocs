0fa9d1381b80d7f516dd2357819f4e1c05946fd7
Szymon Rys <szymonrys@op.pl>
2021-11-01 19:32:21 +0100

    Yes

--- Oblig 7, oop/class_lib.php
+++ Oblig 7, oop/class_lib.php
@@ -1,18 +1,98 @@
 <?php 
-class person {
-    var $szymon;
 
-    fuction set_name($szymon) {
-        $this->name = $szymon;
+class Person {
+    var $navn;
+    var $alder;
+    var $mobil;
+    var $epost;
+
+    function __construct($navn, $alder, $mobil, $epost) {
+        $this->navn = $navn;
+        $this->alder = $alder;
+        $this->mobil = $mobil;
+        $this->epost = $epost;
     }
 
-    function get_name() {
-        return $this->name;
+
+    function get_navn(){
+        return $this->navn;
+    }
+
+    function set_navn($navn){
+        $this->navn = $navn;
+    }
+    
+
+    function get_alder(){
+        return $this->alder;
+    }
+
+    function set_alder($alder){
+        $this->alder = $alder;
+    }
+
+
+    function get_mobil(){
+        return $this->mobil;
+    }
+
+    function set_mobil($mobil){
+        $this->mobil = $mobil;
+    }
+
+
+    function get_epost(){
+        return $this->epost;
+    }
+
+    function set_epost($epost){
+        $this->epost = $epost;
     }
 }
 
+class elev extends Person{
+    private $studentnum;
+    private $elevgruppe;
+    private $kontaktlarer;
 
+    function __construct($person_navn, $person_alder, $person_mobil, $person_epost, $studentnum, $elevgruppe, $kontaktlarer){
+        $this->navn = $person_navn;
+        $this->alder = $person_alder;
+        $this->mobil = $person_mobil;
+        $this->epost = $person_epost;
+        $this->studentnum = $person_studentnum;
+        $this->elevgruppe = $person_elevgruppe;
+        $this->kontaktlarer = $person_kontaktlarer;
+    }
+
+    function set_studentnum($ny_studentnum){
+        $this->studentnum = $ny_studentnum;
+    }
+    
+    function set_elevgruppe($ny_elevgruppe){
+        $this->elevgruppe = $ny_elevgruppe;
+    }
+
+    function set_kontaktlarer($ny_kontaktlarer){
+        $this->kontaktlarer = $ny_kontaktlarer;
+    }
+
+    function get_studentnum(){
+        return $this->studentnum;
+    }
+
+    function get_set_elevgruppe(){
+        return $this->set_elevgruppe;
+    }
+
+    function get_kontaktlarer(){
+        return $this->kontaktlarer;
+    }
+}
 
 ?>
 
+<?php 
 
+
+?>
--- Oblig 7, oop/index.php
+++ Oblig 7, oop/index.php
@@ -9,7 +9,31 @@
 
 <body style="background-color:#6A3B3B;">
     
-<?php include("class_lib.php"); ?>
+<?php 
+include("class_lib.php"); 
+
+$elev1 = new Person("Sander", 18, 78915368, "sanderc@viken.no", "3DAA", "Jon Myrvoll");
+echo "Navn:".$elev1->get_navn(). "<br>";
+echo "Han er:".$elev1->get_alder(). "ar<br>";
+echo "Hans sitt nummer er:".$elev1->get_mobil(). "<br>";
+echo "Mail:".$elev1->get_epost(). "<br></br>";
+#echo "Studentnummeret:".$elev1->get_studentnum(). "<br>";
+
+$elev2 = new Person("Linus", 18, 78915368, "linusg@viken.no", "3DAA", "Jon Myrvoll");
+echo "Navn:".$elev2->get_navn(). "<br>";
+echo "Han er:".$elev2->get_alder(). "ar<br>";
+echo "Hans sitt nummer er:".$elev2->get_mobil(). "<br>";
+echo "Mail:".$elev2->get_epost(). "<br></br>";
+#echo "Studentnummeret:".$elev2->get_studentnum(). "<br>";
+
+$elev3 = new Person("Sevastian", 18, 78915368, "sebastian@viken.no", "3DAA", "Jon Myrvoll");
+echo "Navn:".$elev3->get_navn(). "<br>";
+echo "Han er:".$elev3->get_alder(). "ar<br>";
+echo "Hans sitt nummer er:".$elev3->get_mobil(). "<br>";
+echo "Mail:".$elev3->get_epost(). "<br></br>";
+#echo "Studentnummeret:".$elev3->get_studentnum(). "<br>";
+
+?>
 <h2><p><a href="../index.php">Tilbake til hovedsiden</a></p></h2>  
 </body>
 </html>
