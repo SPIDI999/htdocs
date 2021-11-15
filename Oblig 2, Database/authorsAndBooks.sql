b663f8985da39309792d52760142127335b0f386
SPIDI999 <szymonrys@op.pl>
2021-11-15 09:09:10 +0100

    Yes

--- Oblig 2, Database/authorsAndBooks.sql
+++
@@ -1,3 +0,0 @@
-mysql --user=root --password=
-create database authorsAndBooks;
-use authorsAndBooks;
---
+++ SQL/Oblig 2, Database/authorsAndBooks.sql
@@ -0,0 +1,11 @@
+mysql --user=root --password=
+create database authorsAndBooks;
+use authorsAndBooks;
+create table authors(
+    id int primary key auto_increment,
+    name varchar(60)
+);
+create table books(
+    id int primary key auto_increment,
+    title varchar(60),
+);
