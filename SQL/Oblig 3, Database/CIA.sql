670f7f586b2421ad73fdcf3adb81cd851bcce97a
SPIDI999 <szymonrys@op.pl>
2021-11-23 14:32:31 +0100

    Yes

--- SQL/Oblig 3, Database/CIA.sql
+++ SQL/Oblig 3, Database/CIA.sql
@@ -20,3 +20,4 @@ SELECT * FROM `cia` WHERE `name` LIKE '%ia';
 SELECT * FROM `cia` ORDER BY `name` DESC;
 SELECT `region` FROM `cia` ORDER BY `name` DESC; ? 
 SELECT * FROM `cia` WHERE `population` > 100000000 ORDER BY `name` DESC;
+SELECT `region`, `population`, `name` FROM `cia` ORDER BY `region`,`population` DESC;
